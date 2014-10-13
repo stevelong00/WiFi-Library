842e3ed5c42205d3e3b02014cdc79466eac19688
Steve Long <slong@emaxgaming.com>
2014-10-13 14:16:53 -0400

    Fixed syntax problems

--- build.bat
+++ build.bat
@@ -23,7 +23,7 @@ if "%nuget%" == "" (
 
 
  
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ManagedWifi.sln /p:Configuration="%config% /p:VersionNumber="%version%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ManagedWifi.sln /p:AssemblyVersion="%version%" /p:AssemblyFileVersion="%version%" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
 mkdir Build
 mkdir Build\lib
